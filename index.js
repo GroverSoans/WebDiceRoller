4b197eee6a1c1ef344b872a8fb939f460e6434dd
Grover Soans <groverpsoans@gmail.com>
2024-09-23 16:01:27 -0500

    Basic dice roller completed

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+function rollDice(){
+
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("numOfDice");
+    const diceImages = document.getElementById("diceImages");
+
+    const values = []
+    const images = []
+
+    for(let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random()*6) + 1;
+        //console.log(value);
+        values.push(value);
+        images.push(`<img src="diceImages/${value}.png">`)
+    }
+    //console.log(values)
+
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join('')
+
+}
